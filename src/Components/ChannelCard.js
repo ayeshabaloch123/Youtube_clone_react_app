a5ac5459b2e49a3dd742dd1f7a4e7fcf02f96632
Ayesha Ali <ayeshabaloch278@gmail.com>
2023-08-25 18:52:32 +0500

    Added the Channel Card

--- src/Components/ChannelCard.js
+++ src/Components/ChannelCard.js
@@ -4,8 +4,46 @@ import { Link } from 'react-router-dom';
 import { demoProfilePicture } from '../Utils/Constants';
 import React from 'react';
 
-const ChannelCard = ({ ChannelDetails }) => {
-  return <Box></Box>;
+const ChannelCard = ({ channelDetails }) => {
+  return (
+    <Box
+      sx={{
+        boxShadow: 'none',
+        borderRadius: '20px',
+      }}
+    >
+      <Link to={`/channel/${channelDetails?.id?.channelId}`}>
+        <CardContent
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            textAlign: 'center',
+            color: '#fff',
+          }}
+        >
+          <CardMedia
+            image={
+              channelDetails?.snippet?.thumbnails?.high?.url ||
+              demoProfilePicture
+            }
+            alt={channelDetails?.snippet?.title}
+            sx={{
+              borderRadius: '50%',
+              height: '180px',
+              width: '180px',
+              mb: 2,
+              border: '1px solid #e3e3e3',
+            }}
+          />
+          <Typography variant='h6'>
+            {channelDetails?.snippet?.title}
+            <CheckCircle sx={{ fontSize: '14px', color: 'gray', ml: '5px' }} />
+          </Typography>
+        </CardContent>
+      </Link>
+    </Box>
+  );
 };
 
 export default ChannelCard;
